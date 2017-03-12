cf1a3a9410a555f2664b0dd8e073a8cbccac5a01
terrancexin <xin.terrance@gmail.com>
2017-03-12 16:14:59 -0400

    Add magnifying glass icon to search bar

--- frontend/components/search/search_form.jsx
+++ frontend/components/search/search_form.jsx
@@ -21,6 +21,7 @@ class SearchBar extends React.Component {
       <div className='search-bar'>
         <div className='search-bar-left'>
           <input className='search-input' type='text' placeholder='e.g. tech' value={this.state.name} onChange={this.updateSearch} />
+          <img className='search-icon' src="https://s3.amazonaws.com/meetnow-DEV/meetNow/searchicon.png" alt="search-icon" />
           <div className='within-miles'>within 25 miles by location</div>
         </div>
 
